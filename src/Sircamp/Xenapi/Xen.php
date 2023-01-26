068c940cd12aa1d204252fe79ef2e0dddb110c6e
paul <paul@henz.nl>
2023-01-26 16:51:16 +0100

    Modifing composer file to allow for usage with php 8

--- src/Sircamp/Xenapi/Xen.php
+++ src/Sircamp/Xenapi/Xen.php
@@ -9,66 +9,61 @@ use Sircamp\Xenapi\Element\XenVirtualMachine as XenVirtualMachine;
 class Xen
 {
 
-	private $xenconnection = null;
+    private $xenconnection = null;
 
-	public function __construct($url, $user, $password)
-	{
+    public function __construct($url, $user, $password, $isHttps = true)
+    {
 
 
-		if (!Validator::ip()->validate($url))
-		{
+        if (!Validator::ip()->validate($url)) {
 
-			throw new \InvalidArgumentException("'url' value mast be an ipv4 address", 1);
+            throw new \InvalidArgumentException("'url' value mast be an ipv4 address", 1);
 
-		}
-		if (!Validator::stringType()->validate($user))
-		{
-			throw new \InvalidArgumentException("'user' value mast be an non empty string", 1);
-		}
+        }
+        if (!Validator::stringType()->validate($user)) {
+            throw new \InvalidArgumentException("'user' value mast be an non empty string", 1);
+        }
 
-		if (!Validator::stringType()->validate($password))
-		{
-			throw new \InvalidArgumentException("'password' value mast be an non empty string", 1);
-		}
+        if (!Validator::stringType()->validate($password)) {
+            throw new \InvalidArgumentException("'password' value mast be an non empty string", 1);
+        }
 
-		$this->xenconnection = new XenConnection();
-		try
-		{
-			$this->xenconnection->_setServer($url, $user, $password);
-		}
-		catch (\Exception $e)
-		{
-			die($e->getMessage());
-		}
-	}
+        $this->xenconnection = new XenConnection();
+        try {
+            $url = $isHttps ? "https://{$url}" : $url;
+            $this->xenconnection->_setServer($url, $user, $password);
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
 
-	/**
-	 * Get VM inside Hypervisor from name.
-	 *
-	 * @param mixed $name the name of VM
-	 *
-	 * @return mixed
-	 */
-	public function getVMByNameLabel($name): XenVirtualMachine
-	{
-		$response = new XenResponse($this->xenconnection->VM__get_by_name_label($name));
+    /**
+     * Get VM inside Hypervisor from name.
+     *
+     * @param mixed $name the name of VM
+     *
+     * @return mixed
+     */
+    public function getVMByNameLabel($name): XenVirtualMachine
+    {
+        $response = new XenResponse($this->xenconnection->VM__get_by_name_label($name));
 
-		return new XenVirtualMachine($this->xenconnection, $name, $response->getValue()[0]);
-	}
+        return new XenVirtualMachine($this->xenconnection, $name, $response->getValue()[0]);
+    }
 
-	/**
-	 * Get HOST from name.
-	 *
-	 * @param mixed $name the name of HOST
-	 *
-	 * @return mixed
-	 */
-	public function getHOSTByNameLabel($name)
-	{
-		$response = new XenResponse($this->xenconnection->host__get_by_name_label($name));
+    /**
+     * Get HOST from name.
+     *
+     * @param mixed $name the name of HOST
+     *
+     * @return mixed
+     */
+    public function getHOSTByNameLabel($name)
+    {
+        $response = new XenResponse($this->xenconnection->host__get_by_name_label($name));
 
-		return new XenHost($this->xenconnection, $name, $response->getValue()[0]);
-	}
+        return new XenHost($this->xenconnection, $name, $response->getValue()[0]);
+    }
 
 
 }
